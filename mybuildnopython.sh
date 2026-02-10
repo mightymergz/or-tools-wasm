9fb02a0148bd7031d6e7d74d926ff11a77813933
mightymergz <70487259+mightymergz@users.noreply.github.com>
2026-02-10 12:41:02 -0500

    added various shell scripts as shortcuts to running cmake and modified various cmakelists.txt

---
+++ mybuildnopython.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x
+source /home/mergz/or-tools-wasm/pyodide/emsdk/emsdk/emsdk_env.sh
+#PROJECT=or-tools TARGET=wasm32 tools/cross_compile.sh build --trace-source=CMakeLists.txt --debug-output -DBUILD_PYTHON=ON -DUSE_COINOR=OFF -USE_SCIP=OFF -DBUILD_SAMPLES=OFF -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=NO -DPython3_INCLUDE_DIR=/home/mergz/or-tools-wasm/pyodide/cpython/installs/python-3.12.7/include/python3.12 -DPython_LIBRARY=/home/mergz/or-tools-wasm/pyodide/cpython/installs/python-3.12.7/lib -DCMAKE_TOOLCHAIN_FILE=/home/mergz/or-tools-wasm/pyodide/emsdk/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake 2>&1 | tee wasmoutput.log
+cmake --build build_wasm_nopython --target all -v -j15 2>&1 | tee wasmbuildnopython.log
---
+++ myconf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source /home/mergz/or-tools-wasm/pyodide/emsdk/emsdk/emsdk_env.sh
+#PROJECT=or-tools TARGET=wasm32 tools/cross_compile.sh build --trace-source=CMakeLists.txt --debug-output -DBUILD_PYTHON=ON -DUSE_COINOR=OFF -USE_SCIP=OFF -DBUILD_SAMPLES=OFF -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=NO -DPython3_INCLUDE_DIR=/home/mergz/or-tools-wasm/pyodide/cpython/installs/python-3.12.7/include/python3.12 -DPython_LIBRARY=/home/mergz/or-tools-wasm/pyodide/cpython/installs/python-3.12.7/lib -DCMAKE_TOOLCHAIN_FILE=/home/mergz/or-tools-wasm/pyodide/emsdk/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake 2>&1 | tee wasmoutput.log
+cmake -S. -Bwasm --trace-source=CMakeLists.txt --trace-source=python.cmake --debug-output \
+    -DBUILD_PYTHON=ON -DUSE_COINOR=OFF -USE_SCIP=OFF -DBUILD_SAMPLES=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF \
+    -DPython3_INCLUDE_DIR=/home/mergz/or-tools-wasm/pyodide/cpython/installs/python-3.12.7/include/python3.12 \
+    -DPython_LIBRARY=/home/mergz/or-tools-wasm/pyodide/cpython/installs/python-3.12.7/lib \
+    -DCMAKE_TOOLCHAIN_FILE=/home/mergz/or-tools-wasm/pyodide/pyodide-build/pyodide_build/tools/cmake/Modules/Platform/Emscripten.cmake 2>&1 | tee wasmconf.log
